b23ca62bc7379d2a1d4ee9ce5b3545e406dc61ec
Kacper Padykuła <k.padykula97@gmail.com>
2023-11-05 16:25:40 +0100

    Change some variables for deploy

--- backend/src/core/database/database.provider.ts
+++ backend/src/core/database/database.provider.ts
@@ -5,8 +5,9 @@ export const databaseProviders = [
   {
     provide: CoreConstants.DATABASE_CONNECTION,
     useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect(
-        `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@localhost:27017/${process.env.MONGO_DB}?authMechanism=DEFAULT`,
-      ),
+      mongoose.connect(atlasDBConnectionString),
   },
 ];
+
+const localDBConnectionString = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@localhost:27017/${process.env.MONGO_DB}?authMechanism=DEFAULT`;
+const atlasDBConnectionString = `mongodb+srv://kacper123:kacper123@seeweather.03wg0xh.mongodb.net/?retryWrites=true&w=majority`;
